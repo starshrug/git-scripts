1af4cf0728cf3ab3257ac8ddecab91b2d8155bc1
Sailor <star@sailor.red>
2019-10-21 19:56:00 +0300

    Now you can push to origin!

--- gitmenu.sh
+++ gitmenu.sh
@@ -2,7 +2,7 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 echo -e "Starshrug's trash git menu:"
-echo -e "1.Check status\n2.Track specified file\n3.Track everything\n4.Commit changes\n5.${RED}Cause eternal destruction of everything${NC}\n6.Exit."
+echo -e "1.Check status\n2.Track specified file\n3.Track everything\n4.Commit changes\n5.Push to origin\n6.${RED}Cause eternal destruction of everything${NC}\n7.Exit."
 read a
 case "$a" in
 
@@ -12,15 +12,17 @@ case "$a" in
     read filename
     git add ${filename}
     ;;
-    3)git add .
+    3) git add .
     ;;
-    4)echo "Enter comment:"
+    4) echo "Enter comment:"
     read comment
     git commit -m "${comment}"
     ;;
-    5)rm -rf *
-    git commit -m "I accidentally deleted everything"
+    5) git push origin master
     ;;
-    6) exit 
+    6) rm -rf *
+    git commit -m "I accidentally deleted everything..."
+    ;;
+    7) exit 
     ;;
 esac
