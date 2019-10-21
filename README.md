062d9fb3fd5d8f10483d0a621fea4d184c45e3b1
Sailor <star@sailor.red>
2019-10-21 20:10:13 +0300

    Add README.md and option to edit it.

---
+++ README.md
@@ -0,0 +1,6 @@
+A collection of bash scripts written for git.
+
+1) clone.sh - clones a repository from a given link and cd's into it.
+2) status.sh - Shortened status output, recommended actions and "autopilot"
+3) ipconfig.sh - Checks the entire network for devices
+4) gitmenu.sh - Quick menu for quick actions
--- gitmenu.sh
+++ gitmenu.sh
@@ -2,7 +2,7 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 echo -e "Starshrug's trash git menu:"
-echo -e "1.Check status\n2.Track specified file\n3.Track everything\n4.Add files and commit changes\n5.Push to origin\n6.Exit."
+echo -e "1.Check status\n2.Track specified file\n3.Track everything\n4.Add files and commit changes\n5.Push to origin\n6.Edit readme.md\n7.Exit."
 read a
 case "$a" in
 
@@ -21,7 +21,10 @@ case "$a" in
     ;;
     5) git push origin master
     ;;
-    6) exit 
+    6) nano README.md
+    git add README.md
+    ;;
+    7) exit 
     ;;
     666) echo -e "\n${RED}YOU WILL DELETE ABSOLUTELY EVERYTHING AND WILL NEED TO REVERT, YOU STILL WANT TO DO THIS? [Y]"
     read plsdont
